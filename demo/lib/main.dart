cd504b8b902fa9447ecf4aa32330f5ba12908c7e
komal <komalbhatia442@gmail.com>
2020-11-03 19:10:55 +0530

    Scaffold And AppBar Widget:AppBar:Title:Text

--- demo/lib/main.dart
+++ demo/lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Text('Hey Komolika'),
-));
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text('My First App'),
+    )
+    ,),
+)); 
