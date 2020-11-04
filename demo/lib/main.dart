ab8fe1d7aaede84a28aeff1989a430e346930023
komal <komalbhatia442@gmail.com>
2020-11-04 11:36:03 +0530

    Scaffold:Font:size,style,colour

--- demo/lib/main.dart
+++ demo/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
   home: Scaffold(
-    appBar: AppBar(
+    appBar: AppBar( 
       title: Text('My First App'),
       centerTitle: true ,
       backgroundColor: Colors.red[600],
