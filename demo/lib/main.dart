df568b7e40a5c3bcc68704df20ea141fde26b8e4
komal <komalbhatia442@gmail.com>
2020-11-05 20:35:02 +0530

    Widget LifeCycle:Location function

--- demo/lib/main.dart
+++ demo/lib/main.dart
@@ -1,5 +1,13 @@
+import 'package:demo/pages/Choose_Location.dart';
 import 'package:flutter/material.dart';
 import 'package:demo/pages/home.dart';
+import 'package:demo/pages/loading.dart';
+import 'package:demo/pages/Choose_Location.dart';
 void main() =>runApp(MaterialApp(
-  home: Home(),
+  initialRoute: '/home',
+  routes: {
+    '/':(context)=>Loading(),
+    '/home':(context)=>Home(),
+    '/location':(context)=>ChooseLocation(),
+  },
 ));
