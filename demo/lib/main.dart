a642ac339ec6e14b0bacd4acb911f39106e0aa6d
komal <komalbhatia442@gmail.com>
2020-11-06 15:23:42 +0530

    flutter packages

--- demo/lib/main.dart
+++ demo/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:demo/pages/Choose_Location.dart';
 import 'package:flutter/material.dart';
 import 'package:demo/pages/home.dart';
 import 'package:demo/pages/loading.dart';
-import 'package:demo/pages/Choose_Location.dart';
+
 void main() =>runApp(MaterialApp(
   initialRoute: '/home',
   routes: {
